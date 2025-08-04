e337fa30682587aac67494fb8f75676d7b3f0518
Uchechukwu-Udefi <udefihenry@gmail.com>
2025-08-04 21:01:26 +0200

    Create server file and update projects

---
+++ my-portfolio/server.js
@@ -0,0 +1,74 @@
+export const projects = [
+    {
+        id: 1,
+        title: "Portfolio Website",
+        image: "my-portfolio/src/assets/logo-1.png",
+        description: "A personal portfolio website showcasing my skills, certifications, projects. and contact/socials",
+        date: "April 2024",
+        video: " ",
+        technologies: ["HTML", "CSS", "React", "Vite", "Node.js"],
+        liveDemo: "https://uchechukwu-udefi-2024ca.netlify.app/",
+        githubLink: "https://github.com/Uchechukwu-Udefi/UCHUDE24034_CPTO2409_Group-A_Uchechukwu-Udefi_CDV05",
+        status: "Completed".
+    },
+    {
+        id: 2,
+        title: "Podcast App",
+        image: "my-portfolio/src/assets/u-tune_logo_3.png",
+        description: "A full-featured React web application for browsing, viewing, and managing podcast shows. Users can search shows, explore by genre, view detailed season and episode info, manage favorite shows, view and manage listening history, and enjoy in-app audio playback — all within a seamless experience using modern React features and routing.",
+        date: "July 2025",
+        video: "https://www.loom.com/share/9f1300d4c1b44a5ca9569f4e5d3c35d8?sid=bc8332c2-3666-41ba-a16d-7465e653d9a2",
+        technologies: ["HTML", "CSS", "React", "Vite", "Node.js"],
+        liveDemo: "https://u-tune.netlify.app/",
+        githubLink: "https://github.com/Uchechukwu-Udefi/UCHUDE24034_CPTO2409_Group-A_Uchechukwu-Udefi_DJS11",
+        status: "Completed".
+    },
+    {
+        id: 3,
+        title: "Cache Bank Landing Page",
+        image: "my-portfolio/src/assets/cache-bank-logo.png",
+        description: "A Cache Bank Landing Page that displays the bank's services, features, and contact information. It includes a hero section with endorsing company logos, service cards, testimonials, contact details, and footer with copyright",
+        date: "November 2024",
+        video: " ",
+        technologies: ["HTML", "Tailwind CSS", "JavaScript"],
+        liveDemo: "",
+        githubLink: "https://github.com/Uchechukwu-Udefi/Uchechukwu-Udefi-SDF_Portfolio_Piece_UCHUDE24034_CPTO2409_Group-A_Uchechukwu-Udefi_SDF11",
+        status: "In Progress".
+    },
+    {
+        id: 4,
+        title: "Project Three",
+        image: "my-portfolio/src/assets/cache-bank-logo.png",
+        description: "This is the third project.",
+        date: " ",
+        video: " ",
+        technologies: ["Python", "Django"],
+        liveDemo: "",
+        githubLink: "https://github.com/user/project-three",
+        status: "done".
+    },
+    {
+        id: 5,
+        title: "Project Three",
+        image: "my-portfolio/src/assets/cache-bank-logo.png",
+        description: "This is the third project.",
+        date: " ",
+        video: " ",
+        technologies: ["Python", "Django"],
+        liveDemo: "",
+        githubLink: "https://github.com/user/project-three",
+        status: "done".
+    },
+    {
+        id: 6,
+        title: "Project Three",
+        image: "my-portfolio/src/assets/cache-bank-logo.png",
+        description: "This is the third project.",
+        date: " ",
+        video: " ",
+        technologies: ["Python", "Django"],
+        liveDemo: "",
+        githubLink: "https://github.com/user/project-three",
+        status: "done".
+    },
+];
