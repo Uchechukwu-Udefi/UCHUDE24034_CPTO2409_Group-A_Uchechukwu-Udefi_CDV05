4ce443f3de739ec4c0d598fc7d3d145127924f8a
Uchechukwu-Udefi <udefihenry@gmail.com>
2025-10-29 11:15:03 +0200

    Add podcast project card

--- my-portfolio/src/pages/Contact.jsx
+++
@@ -1,8 +0,0 @@
-export default function Contact() {
-    return (
-        <div>
-            <h1>Contact Me</h1>
-            <p>If you would like to get in touch, please reach out via email.</p>
-        </div>
-    );
-}
--- my-portfolio/src/pages/ProjectDetails.jsx
+++
@@ -1,8 +0,0 @@
-export default function ProjectDetails() {
-    return (
-        <div>
-            <h1>Project Details</h1>
-            <p>Here you can find more information about each project.</p>
-        </div>
-    );
-}
